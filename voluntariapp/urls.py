9516a4f458d2047c13c83e0ba15dfe8fd2e8705d
Elena <eizquierdograu@gmail.com>
2019-12-05 15:57:43 +0100

    add everything like before but using User and not using Token authentication

--- voluntariapp/urls.py
+++ voluntariapp/urls.py
@@ -8,4 +8,18 @@ urlpatterns = [
 
     path('event', views.EventListView.as_view()),
     path('event/<id_event>', views.EventDetailView.as_view(), name="event-details"),
+
+    path('forum', views.ForumThemeListView.as_view(), name="forum"),
+    path('forum/<id_forumtheme>', views.ForumThemeDetailView.as_view(), name="forumtheme-detail"),
+
+    path('comment', views.ListCommentView.as_view(), name="comments-all"),
+    path('comment/<id_comment>', views.CommentDetailView.as_view(), name="comments-detail"),
+    path('comment/forum/<id_forumtheme>', views.CommentFromThemeView.as_view(), name="comments-from-theme"),
+
+    path('rate', views.ListRateView.as_view(), name="rate-all"),
+    path('rate/<id_rate>', views.RateDetailView.as_view(), name="rate-detail"),
+    path('rate/event/<id_event>', views.RateFromEventView.as_view(), name="rate-from-event"),
+
+    path('eventattendee', views.ListEventAttendeeView.as_view(), name="eventattendee-all"),
+    path('event/<id_event>/attendee', views.AttendeeView.as_view(), name="eventattendee-detail"),
 ]
