62755c0fc9ddce9bed2439b4f03875dd9ff42381
Elena <eizquierdograu@gmail.com>
2019-12-17 17:55:12 +0100

    remove corsheaders

--- voluntariapp_api/settings.py
+++ voluntariapp_api/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'voluntariapp',
-    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -51,7 +50,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'voluntariapp_api.urls'
