44bd0fc825864108b8bceecb2abef01241e6e8d9
Elena <eizquierdograu@gmail.com>
2019-12-18 12:29:17 +0100

    add pagination for explicacions

--- voluntariapp/serializers.py
+++ voluntariapp/serializers.py
@@ -5,12 +5,14 @@ from rest_framework import serializers
 from . import models
 import pytz
 
-utc=pytz.UTC
+utc = pytz.UTC
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields='__all__'
+        fields = '__all__'
+
 
 class EventSerializer(serializers.ModelSerializer):
     attending = serializers.SerializerMethodField()
@@ -22,70 +24,84 @@ class EventSerializer(serializers.ModelSerializer):
 
     def get_attendance(self, obj):
         return models.EventAttendee.objects.filter(event=obj).count()
+
     class Meta:
         model = models.Event
-        fields = ('id','creator','title','group','start_date','end_date','description','attendance','attending')
+        fields = ('id', 'creator', 'title', 'group', 'start_date', 'end_date', 'description', 'attendance', 'attending')
 
-#FALTA ATTENDING!
+
+# FALTA ATTENDING!
 class EventGetSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Event
-        fields='__all__'
+        fields = '__all__'
+
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Comment
-        fields = ("id", "author","content","created_date", "forumtheme")
+        fields = ("id", "author", "content", "created_date", "forumtheme")
+
 
 class ForumThemeGetSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.ForumTheme
-        fields = ("id","title","creator","description", "finished", "created_date", "group")
+        fields = ("id", "title", "creator", "description", "finished", "created_date", "group")
+
 
 class ForumThemeSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.ForumTheme
-        fields = ("id","title","creator","description", "finished", "created_date", "group")
+        fields = ("id", "title", "creator", "description", "finished", "created_date", "group")
+
 
 class RateSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Rate
         fields = ("event", "comments", "snack_rate", "line_rate", "circle_rate", "respect_rate", "activity_rate")
 
+
 class EventAttendeeSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.EventAttendee
         fields = ("id", "user", "event")
 
+
 class CoursSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Cours
-        fields='__all__'
+        fields = '__all__'
+
 
 class QuarterSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Quarter
         fields = '__all__'
 
+
 class WeekSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Week
         fields = '__all__'
+
 
 class CentreInteresSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.CentreInteres
         fields = '__all__'
 
+
 class ObjectiuSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Objectiu
         fields = '__all__'
 
+
 class ExplicacioSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Explicacio
         fields = '__all__'
+
 
 class ExplicacioGetFromCentreInteresSerializer(serializers.ModelSerializer):
     finished = serializers.SerializerMethodField()
--- voluntariapp/views_directory/explicacio_views.py
+++ voluntariapp/views_directory/explicacio_views.py
@@ -3,11 +3,13 @@ from rest_framework import generics
 from rest_framework.generics import get_object_or_404
 from rest_framework.parsers import MultiPartParser, JSONParser
 from voluntariapp.models import Explicacio
+from voluntariapp.paginations import OneByOneItems
 from voluntariapp.serializers import ExplicacioSerializer, ExplicacioGetFromCentreInteresSerializer
 from rest_framework import status
 from rest_framework.response import Response
 from django.utils import timezone
 from django.http import JsonResponse
+
 
 class ListExplicacioView(generics.ListAPIView):
     queryset = Explicacio.objects.all()
@@ -17,7 +19,7 @@ class ListExplicacioView(generics.ListAPIView):
     def get(self, request, *args, **kwargs):
         queryset = Explicacio.objects.all()
         serializer = ExplicacioSerializer(queryset, many=True, context={'request': request})
-        return Response(data=serializer.data,status=status.HTTP_200_OK)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request):
         serializer = ExplicacioSerializer(data=request.data)
@@ -25,6 +27,15 @@ class ListExplicacioView(generics.ListAPIView):
             serializer.save()
             return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
         return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+def post(self, request):
+    serializer = ExplicacioSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
+    return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class ExplicacioDetailView(generics.RetrieveUpdateDestroyAPIView):
     """
@@ -37,7 +48,7 @@ class ExplicacioDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = ExplicacioSerializer
 
     def get(self, request, id_explicacio):
-        a_explicacio = get_object_or_404(Explicacio,pk=id_explicacio)
+        a_explicacio = get_object_or_404(Explicacio, pk=id_explicacio)
         serializer = ExplicacioSerializer(a_explicacio, context={'request': request})
         return Response(data=serializer.data, status=status.HTTP_200_OK)
 
@@ -53,12 +64,18 @@ class ExplicacioDetailView(generics.RetrieveUpdateDestroyAPIView):
         a_explicacio.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-class ExplicacioFromCentreInteresView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Explicacio.objects.all()
+
+class ExplicacioFromCentreInteresView(generics.ListAPIView):
+    queryset = Explicacio.objects.all().order_by('date')
     parser_classes = (MultiPartParser, JSONParser)
     serializer_class = ExplicacioGetFromCentreInteresSerializer
 
     def get(self, request, id_centreinteres):
-        explicacions = self.queryset.filter(centreinteres=id_centreinteres)
-        serializer = ExplicacioGetFromCentreInteresSerializer(explicacions, many=True, context={'request': request})
-        return Response(data=serializer.data, status=status.HTTP_200_OK)
+        self.pagination_class = OneByOneItems
+        queryset = self.filter_queryset(self.queryset.filter(centreinteres=id_centreinteres))
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
